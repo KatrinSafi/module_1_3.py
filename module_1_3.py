f24ea3a58b1f178c6f706509c37eea22244a6864
KatrinSafi <167858422+KatrinSafi@users.noreply.github.com>
2024-10-05 21:20:44 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+name = "Ekaterina"
+print (name, type (name))
+age = 36
+print (age, type (age))
+new_age = 36 + 2
+print(new_age, type (age))
+name = "Is_Student"
+print (name,type (name))
+name = True
+print (name, type (name))
